e46a324ad3f586da1b31b2625d53ac115b99d220
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-14 17:10:44 +0530

    [sai] Condition when player moves below 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,33 +1,43 @@
 public class SnakeLadder {
 
    public static final int startPosition = 0;
+   public static final int winningPosition = 100;
 	public static void main(String[] args) {
       int dieRoll;
       int currentPosition = 0;
       int playerPosition = startPosition;
 		dieRoll = (int)(Math.random()*6) + 1;
-      
+      //int option = (int)(Math.random()*3);
 
-      System.out.println("The die comes up " + dieRoll);
-      //System.out.println("Player position is: " + Position);
+      //System.out.println("The die comes up " + dieRoll);
+      while (playerPosition != winningPosition)
+      {
+          System.out.println("The die comes up " + dieRoll);
+          currentPosition = playerPosition;
+          int option = (int)(Math.random()*3);
+          if(option==0)
+          {
+             System.out.println("No Play");
+             playerPosition = currentPosition;
 
-      int option = (int)(Math.random()*3);
-      if(option==0)
-      {
-          System.out.println("No Play");
-          currentPosition = startPosition;
 
+          }
+          else if(option==1)
+          {
+             System.out.println("Got Ladder");
+             playerPosition += dieRoll;
+          }
+          else if(option==2)
+          {
+             System.out.println("Snake attacked");
+             playerPosition -= dieRoll;
+             if(playerPosition <= 1)
+             {
+                   playerPosition = 1;
+             }
+
+          }
+          System.out.println("New Player position is: " + playerPosition);
       }
-      else if(option==1)
-      {
-          System.out.println("Got Ladder");
-          currentPosition += dieRoll;
-      }
-      else if(option==2)
-      {
-          System.out.println("Snake attacked");
-          currentPosition -= dieRoll;
-      }
-      System.out.println("New Player position is: " + currentPosition);
 	}
 }
