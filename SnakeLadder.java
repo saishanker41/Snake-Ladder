bea56a0e6bcf592c6892f8d0cab2d55ed252ba0e
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-14 15:37:28 +0530

    [sai] Options are arranged in the game

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,12 +3,31 @@ public class SnakeLadder {
    public static final int startPosition = 0;
 	public static void main(String[] args) {
       int dieRoll;
+      int currentPosition = 0;
       int playerPosition = startPosition;
 		dieRoll = (int)(Math.random()*6) + 1;
-      playerPosition += dieRoll;
+      
+
       System.out.println("The die comes up " + dieRoll);
+      //System.out.println("Player position is: " + Position);
 
-      System.out.println("Player position is: " + playerPosition);
+      int option = (int)(Math.random()*3);
+      if(option==0)
+      {
+          System.out.println("No Play");
+          currentPosition = startPosition;
 
+      }
+      else if(option==1)
+      {
+          System.out.println("Got Ladder");
+          currentPosition += dieRoll;
+      }
+      else if(option==2)
+      {
+          System.out.println("Snake attacked");
+          currentPosition -= dieRoll;
+      }
+      System.out.println("New Player position is: " + currentPosition);
 	}
 }
