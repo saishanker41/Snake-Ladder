e26e4bc4c7e649ad9360eb8304bcc0629fbde92a
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-14 18:32:46 +0530

    [sai] one more player added in the game

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,53 +3,94 @@ public class SnakeLadder {
    public static final int startPosition = 0;
    public static final int winningPosition = 100;
 	public static void main(String[] args) {
-      int dieRoll;
-      int dieRollCount = 0;
+      int dieRoll1, dieRoll2;
+      int dieRollCount1 = 0, dieRollCount2 = 0;
       //int currentPosition = 0;
-      int playerPosition = 0;
+      int playerPosition1 = 0;
+      int playerPosition2 = 0;
 		//dieRoll = (int)(Math.random()*6) + 1;
       //int option = (int)(Math.random()*3);
 
       //System.out.println("The die comes up " + dieRoll);
-      while (playerPosition < winningPosition)
+      while ((playerPosition1 < winningPosition) && (playerPosition2 < winningPosition))
       {
-          dieRollCount++;
-          System.out.println("Dice count is: " + dieRollCount);
-          dieRoll = (int)(Math.random()*6) + 1;
-          System.out.println("The die comes up " + dieRoll);
-          int option = (int)(Math.random()*3);
-          if(option==0)
+          dieRollCount1++;
+          System.out.println("Dice count is: " + dieRollCount1);
+          dieRoll1 = (int)(Math.random()*6) + 1;
+          System.out.println("The die comes up " + dieRoll1);
+          int option1 = (int)(Math.random()*3);
+          if(option1==0)
           {
              System.out.println("No Play");
-             playerPosition = playerPosition;
+             playerPosition1 = playerPosition1;
 
 
           }
-          else if(option==1)
+          else if(option1==1)
           {
              System.out.println("Got Ladder");
-             playerPosition += dieRoll;
-             if(playerPosition > 100)
+             playerPosition1 += dieRoll1;
+             if(playerPosition1 > 100)
              {
-                   playerPosition -= dieRoll;
+                   playerPosition1 -= dieRoll1;
              }
 
           }
-          else if(option==2)
+          else if(option1==2)
           {
              System.out.println("Snake attacked");
-             playerPosition -= dieRoll;
-             if(playerPosition < 0)
+             playerPosition1 -= dieRoll1;
+             if(playerPosition1 < 0)
              {
-                   playerPosition = 0;
+                   playerPosition1 = 0;
              }
 
           }
-          System.out.println("New Player position is: " + playerPosition);
-          if(playerPosition == 100)
+          System.out.println("New Player1 position is: " + playerPosition1);
+
+          //player2
+          dieRollCount2++;
+          System.out.println("Dice count is: " + dieRollCount2);
+          dieRoll2 = (int)(Math.random()*6) + 1;
+          System.out.println("The die comes up " + dieRoll2);
+          int option2 = (int)(Math.random()*3);
+          if(option2==0)
           {
-             System.out.println("Hurray!... You have reached to Winning Position");
+             System.out.println("No Play");
+             playerPosition2 = playerPosition2;
+
+
           }
+          else if(option2==1)
+          {
+             System.out.println("Got Ladder");
+             playerPosition2 += dieRoll2;
+             if(playerPosition2 > 100)
+             {
+                   playerPosition2 -= dieRoll2;
+             }
+
+          }
+          else if(option2==2)
+          {
+             System.out.println("Snake attacked");
+             playerPosition2 -= dieRoll2;
+             if(playerPosition2 < 0)
+             {
+                   playerPosition2 = 0;
+             }
+
+          }
+          System.out.println("New Player2 position is: " + playerPosition2);
       }
+      if(playerPosition1  == winningPosition)
+      {
+          System.out.println("Hurray!... Player1 has reached to Winning Position in " + dieRollCount1 + " steps");
+      }
+      else
+      {
+          System.out.println("Huuray!... Player2 has reached to winning Position in " + dieRollCount2 + " steps");
+      }
+
 	}
 }
