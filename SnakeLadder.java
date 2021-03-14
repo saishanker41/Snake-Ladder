6cda3a6d85f41a1f58ffca3e73b15bec15d5fe70
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-14 13:56:47 +0530

    [sai] Added start position of a player

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,10 @@
 public class SnakeLadder {
 
+   public static final int startPosition = 0;
 	public static void main(String[] args) {
 		System.out.println("File for initial commit");
+      System.out.println("Welcome to Snake and Ladder Game");
+      System.out.println("Start position is: " + startPosition);
+
 	}
 }
