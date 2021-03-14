8c0e2cc2581ff60ebb61fa9ba6f66ea29d563201
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-14 17:36:10 +0530

    [sai] Added when player reach 100 it will show u a msg

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,21 +4,21 @@ public class SnakeLadder {
    public static final int winningPosition = 100;
 	public static void main(String[] args) {
       int dieRoll;
-      int currentPosition = 0;
-      int playerPosition = startPosition;
-		dieRoll = (int)(Math.random()*6) + 1;
+      //int currentPosition = 0;
+      int playerPosition = 0;
+		//dieRoll = (int)(Math.random()*6) + 1;
       //int option = (int)(Math.random()*3);
 
       //System.out.println("The die comes up " + dieRoll);
-      while (playerPosition != winningPosition)
+      while (playerPosition < winningPosition)
       {
+          dieRoll = (int)(Math.random()*6) + 1;
           System.out.println("The die comes up " + dieRoll);
-          currentPosition = playerPosition;
           int option = (int)(Math.random()*3);
           if(option==0)
           {
              System.out.println("No Play");
-             playerPosition = currentPosition;
+             playerPosition = playerPosition;
 
 
           }
@@ -28,7 +28,7 @@ public class SnakeLadder {
              playerPosition += dieRoll;
              if(playerPosition > 100)
              {
-                   playerPosition = currentPosition;
+                   playerPosition -= dieRoll;
              }
 
           }
@@ -36,13 +36,17 @@ public class SnakeLadder {
           {
              System.out.println("Snake attacked");
              playerPosition -= dieRoll;
-             if(playerPosition <= 1)
+             if(playerPosition < 0)
              {
-                   playerPosition = 1;
+                   playerPosition = 0;
              }
 
           }
           System.out.println("New Player position is: " + playerPosition);
+          if(playerPosition == 100)
+          {
+             System.out.println("Hurray!... You have reached to Winning Position");
+          }
       }
 	}
 }
