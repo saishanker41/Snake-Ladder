00d5c03ebeade9f182d03c0908dc29f331ef3b39
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2021-03-14 14:31:03 +0530

    [sai] Rolled Dice and placed player position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,9 +2,13 @@ public class SnakeLadder {
 
    public static final int startPosition = 0;
 	public static void main(String[] args) {
-		System.out.println("File for initial commit");
-      System.out.println("Welcome to Snake and Ladder Game");
-      System.out.println("Start position is: " + startPosition);
+      int dieRoll;
+      int playerPosition = startPosition;
+		dieRoll = (int)(Math.random()*6) + 1;
+      playerPosition += dieRoll;
+      System.out.println("The die comes up " + dieRoll);
+
+      System.out.println("Player position is: " + playerPosition);
 
 	}
 }
